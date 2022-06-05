1fe965040ad7e2677ed112444c0ff8eb75880b95
Radhiansya Zain Antriksa Putra <radhi.rz666@gmail.com>
2022-06-05 16:23:50 +0700

    feat: add route to predict

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+from .ml_model import predict_sentences
+from .utils import load_model, load_tokenizer
+
+class MessagesReq(BaseModel):
+    messages: str
 
 app = FastAPI()
+
+model = load_model('./model/chatbot-model.h5')
+tokenizer = load_tokenizer('./model/tokenizer.pickle')
 
 origins = ['*']
 
@@ -17,3 +27,12 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"message": "Hello Bangkit"}
+
+@app.post("/predict/")
+async def predict_label(req: MessagesReq):
+    PADDING = 'post'
+    MAXLEN = 15
+
+    result = predict_sentences([req.messages], model, tokenizer, PADDING, MAXLEN)
+
+    return {"label": result}
